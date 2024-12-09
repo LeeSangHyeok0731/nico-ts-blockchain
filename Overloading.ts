7cd2e42159589ad3d614d9d1cb39b573685bdbea
fakesanghyeok <s24066@gsm.hs.kr>
2024-12-09 19:48:52 +0900

    Overloading

--- Overloading.ts
+++ Overloading.ts
@@ -1,6 +1,12 @@
 type Add = {
   (a: number, b: number): number;
-  (a: number, b: string): number;
+  (a: number, b: number, c: number): number;
 };
 
-const add: Add = (a, b) => a + b;
+const add: Add = (a, b, c?: number) => {
+  if (c) return a + b + c;
+  return a + b;
+};
+
+add(1, 2);
+add(1, 2, 3);
